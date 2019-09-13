e924398b2e570b8d541becaea7b1eac5c4317383
swonb <swonbinca@gmail.com>
2019-09-12 22:42:02 -0400

    + Redux

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/postActions';
 
 class Postform extends Component {
     constructor(props) {
@@ -24,15 +27,18 @@ class Postform extends Component {
             body: this.state.body
         }
 
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(post)
-        })
-            .then(res => res.json())
-            .then(data => console.log(data));
+        // fetch('https://jsonplaceholder.typicode.com/posts', {
+        //     method: 'POST',
+        //     headers: {
+        //         'content-type': 'application/json'
+        //     },
+        //     body: JSON.stringify(post)
+        // })
+        //     .then(res => res.json())
+        //     .then(data => console.log(data));
+
+        // Call Action
+        this.props.createPost(post);
     }
 
     render() {
@@ -57,4 +63,8 @@ class Postform extends Component {
     }
 }
 
-export default Postform;
+Postform.propTypes = {
+    createPost: PropTypes.func.isRequired
+}
+
+export default connect(null, { createPost })(Postform);
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
 
 class Posts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: []
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         posts: []
+    //     }
+    // }
+
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/posts')
+    //         .then(res => res.json())
+    //         .then(data => this.setState({ posts: data }));
+    // }
+
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.newPost) {
+            this.props.posts.unshift(nextProps.newPost);
         }
     }
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(res => res.json())
-            .then(data => this.setState({ posts: data }));
-    }
-
     render() {
-        const postItems = this.state.posts.map(post => (
+        const postItems = this.props.posts.map(post => (
             <div key={post.id}>
                 <h3>{post.title}</h3>
                 <p>{post.body}</p>
@@ -31,4 +44,15 @@ class Posts extends Component {
     }
 }
 
-export default Posts;
+Posts.propTypes = {
+    fetchPosts: PropTypes.func.isRequired,
+    posts: PropTypes.array.isRequired,
+    newPost: PropTypes.object
+}
+
+const mapStateToProps = state => ({
+    posts: state.posts.items,
+    newPost: state.posts.item
+})
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
